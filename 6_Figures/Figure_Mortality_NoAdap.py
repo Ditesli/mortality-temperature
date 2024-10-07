ced7b2885f6afe7631e8249759420912d5cfd89e
Ditesli <ditesli@live.com>
2024-10-07 18:44:13 +0200

    Update Figure_Mortality_NoAdap.py

--- 6_Figures/Figure_Mortality_NoAdap.py
+++ 6_Figures/Figure_Mortality_NoAdap.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import os, sys
+sys.path.append(os.path.join(os.getcwd(), '0_Params')) # Append the path
+import climate_models_info
 
-base_path = 'C:/Users/Nayeli/Documents' ### Select main folder
+base_path = 'C:/Users/Nayeli/Documents' ### Select path to main folder
+folder_path = f'{base_path}/Figures '
+if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
 ''' Define important variables '''
 colors = {'SSP126': '#5ea7cd', 'SSP245': '#33416b', 'SSP370': '#dd8852', 'SSP585': '#c04242'}
@@ -11,23 +16,21 @@ labels = ['2020', '2040', '2060', '2080', '2100']
 scenarios = ['SSP126', 'SSP245', 'SSP370', 'SSP585']
 decades = ['2015','2020','2025','2030','2035','2040','2045','2050','2055','2060','2065','2070','2075','2080', '2085','2090','2095','2100']
 groups = ['Oldest (age > 64 years)', 'Older (5-64 years)', 'Young (age < 5 years)']
-climate_models = ['AWI-CM-1-1-MR', 'BCC-CSM2-MR', 'CAMS-CSM1-0', 'CESM2', 'CESM2-WACCM', 'CMCC-CM2-SR5', 'CMCC-ESM2', 'CNRM-CM6-1-HR', 'EC-Earth3',
-                  'EC-Earth3-Veg', 'GFDL-ESM4', 'INM-CM4-8', 'INM-CM5-0', 'MPI-ESM1-2-HR', 'MRI-ESM2-0', 'NorESM2-MM', 'TaiESM1']  
+scenario_SSP = 'SSP2'
 dataframes = {}
-for climate_model in climate_models:
-    file_path = f'No adaptation files - Climate models/Total mortality_No adaptation_{climate_model}.csv'
-    # file_path = f'No adaptation files - Climate models/Total mortality_No adaptation_SSP2_{climate_model}.csv'
+for climate_model in climate_models_info.climate_models_dic.keys():
+    file_path = f'{base_path}/Main folder/Mortality/No Adaptation/TotalMortality_NoAdaptation_{climate_model}.csv'
+    # file_path = f'{base_path}/Main folder/Mortality/No Adaptation/TotalMortality_NoAdaptation_{scenario_SSP}_{climate_model}.csv'
     dataframes[climate_model] = pd.read_csv(base_path+file_path, index_col=[0,1])
 
 ''' Calculate mean and stdev '''
 index = pd.MultiIndex.from_product([groups, scenarios], names=['Age group', 'Scenario'])
 average = pd.DataFrame(index=index, columns=decades)
 stdev = pd.DataFrame(index=index, columns=decades)
-
 for i in np.arange(0,12):
     for j in range(len(decades)):
         a = []
-        for climate_model in climate_models:
+        for climate_model in climate_models_info.climate_models_dic.keys():
             a.append(dataframes[climate_model].iloc[i,j])
         average.iloc[i,j] = np.mean(a)
         stdev.iloc[i,j] = 2*np.std(a)
@@ -72,7 +75,7 @@ for i, group in enumerate(groups, start=1):
 
 plt.suptitle('Global mortality projections without adaptation', fontsize=12, y=0.95, weight='bold')
 # plt.suptitle('Global mortality projections without adaptation - SSP2 population', fontsize=12, y=0.95, weight='bold')
-plt.savefig(os.path.join(os.getcwd(), '6_Figures', 'Fig_Mortality_Global_NoAdap.png'), bbox_inches='tight', dpi=300)
-# plt.savefig(os.path.join(os.getcwd(), '6_Figures', 'Fig_Mortality_Global_NoAdap_SSP2.png'), bbox_inches='tight', dpi=300)
-plt.show()
+plt.savefig(f'{folder_path}/Fig_Mortality_Global_NoAdap.png'), bbox_inches='tight', dpi=300)
+# plt.savefig(f'{folder_path}_Fig_Mortality_Global_NoAdap_SSP2.png'), bbox_inches='tight', dpi=300)
+#splt.show()
 
