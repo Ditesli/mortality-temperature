52cdd5c677018f67a6bef89a906215b0584684e8
Ditesli <ditesli@live.com>
2024-11-07 14:07:48 +0100

    MIMOSA Scatterplots complete

--- 0_Params/MIMOSA_params.py
+++ 0_Params/MIMOSA_params.py
@@ -1,3 +1,6 @@
+from scipy.optimize import curve_fit
+import numpy as np
+
 MIMOSA = {
     'Canada': ['CAN'],
     'USA': ['SPM', 'USA', 'UMI'],
@@ -47,3 +50,76 @@ region_colors = {'Brazil':0, 'Canada':1, 'Central America':3, 'Central Asia': 4,
                  'Oceania': 17, 'Rest of South America': 18, 'Rest of South Asia': 19, 'Rest of Southern Africa': 20, 'Russia region': 21, 
                  'South Africa': 22, 'Southeastern Asia': 23, 'Turkey': 24, 'USA': 25, 'Ukraine region': 26, 'Western Africa': 27, 'Western Europe': 14,
                  'WORLD':36}
+
+
+
+def quadratic(x, a, b, c):
+    return a*x**2 + b*x + c
+
+def count_points_above(y_fit, y, alpha):
+            new_y_fit = alpha * y_fit
+            return np.sum(y > new_y_fit)
+
+def find_alpha(y_fit, y, target_points, tol=1, max_iter=200):
+    mid = 1.0; high = 20; low = -20; iteration = 0
+
+    while iteration < max_iter:
+        iteration += 1
+        points_above = count_points_above(y_fit, y, mid)
+    
+        if abs(points_above - target_points) < tol:
+            break
+
+        if points_above > target_points:
+            low = mid
+        else:
+            high = mid
+
+        mid = (high + low) / 2.0
+
+    return mid
+
+def order_data(x, y):
+    sorted_indices = np.argsort(x)
+    return x[sorted_indices], y[sorted_indices]
+
+# Función para encontrar los índices de los valores más cercanos
+def find_indices_within_interval(x, start, end):
+    indices = np.where((x >= start) & (x < end))[0]
+    return indices
+
+def coeff_percentile(x, y, lower_bound, upper_bound):
+
+    x_ordered, y_ordered = order_data(x,y)
+    
+    quantile_10_x = []; quantile_10_y = []; quantile_90_x = []; quantile_90_y = []
+    
+    # Recorrer los intervalos y encontrar los valores correspondientes
+    intervals = [(i, i + 1) for i in range(7)]
+    for start, end in intervals:
+        indices = find_indices_within_interval(x_ordered, start, end)
+        if indices.size > 0:
+            y_values = y_ordered[indices]
+            x_values = x_ordered[indices]
+            
+            q10 = np.quantile(y_values, lower_bound)
+            q90 = np.quantile(y_values, upper_bound)
+            
+            # Encontrar los índices de los quantiles
+            q10_index = np.argmin(np.abs(y_values - q10))
+            q90_index = np.argmin(np.abs(y_values - q90))
+            
+            # Almacenar los valores correspondientes
+            quantile_10_x.append(x_values[q10_index])
+            quantile_10_y.append(y_values[q10_index])
+            quantile_90_x.append(x_values[q90_index])
+            quantile_90_y.append(y_values[q90_index])
+    
+    # Convertir las listas a arrays
+    quantile_10_x = np.array(quantile_10_x); quantile_10_y = np.array(quantile_10_y)
+    
+    quantile_90_x = np.array(quantile_90_x); quantile_90_y = np.array(quantile_90_y)
+    
+    popt_10, _ = curve_fit(quadratic, quantile_10_x, quantile_10_y)
+    popt_90, _ = curve_fit(quadratic, quantile_90_x, quantile_90_y)
+    return popt_10, popt_90
--- 5_MIMOSA/1_MIMOSA_Mortality calculation.py
+++ 5_MIMOSA/1_MIMOSA_Mortality calculation.py
@@ -14,17 +14,48 @@ if not os.path.exists(folder_path):
 
 '''Define important variables'''
 scenarios = ['SSP126', 'SSP245', 'SSP370', 'SSP585']
+scenarios_SSP = ['SSP1', 'SSP2', 'SSP3', 'SSP5']
 scenario_SSP = 'SSP2' ### Results for MIMOSA assume a Middle-of-the-Road population growth
-years = np.arange(2015,2105,5)
+years = np.arange(2015,2025,5)
 groups = ['oldest', 'older', 'young']
-results = pd.DataFrame(columns = pd.MultiIndex.from_product([groups, scenarios, years], names=['Age group', 'Scenario', 'Year']))
+results = pd.DataFrame(index=MIMOSA_params.MIMOSA.keys(), columns = pd.MultiIndex.from_product([groups, scenarios, years], names=['Age group', 'Scenario', 'Year']))
 
-'''Calculate regional mortality'''
+
+'''Calculate regional mortality all POP scenarios'''
+for climate_model in climate_models_info.climate_models_dic.keys():
+    for group in groups:
+        mor_np = calculate_mortality_noadap.read_mortality_response(RF_path, group)
+        for scenario, scenario_SSP in zip(scenarios, scenarios_SSP):
+            # Read population files
+            POP = pd.read_csv(f'{base_path}/Main folder/GDP & POP/POP files/POP_{scenario_SSP}_{group}.csv')
+            POP['MIMOSA'] = POP['hierid'].apply(MIMOSA_params.get_region)
+            POP_MIMOSA = POP[[f"{num}" for num in years] + ['MIMOSA']].groupby('MIMOSA').sum()
+            for year in years:
+                #SSP = pd.read_csv(f'{base_path}/Climate data/Climate ensemble/{climate_model}/{scenario}/BC_{climate_model}_{scenario}_{year}.csv')  # Read temp files
+                SSP = pd.read_csv(f'D:\\Climate models - Bias Corrected\\{climate_model}\\{scenario}\\BC_{climate_model}_{scenario}_{year}.csv')
+                 # Calculate relative mortality per impact region
+                mor_temp = calculate_mortality_noadap.calculate_mortality_year(SSP, mor_np)
+                 # Calculate total mortality per impact region
+                mor_temp['total'] = POP[f'{year}'] * mor_temp['total mortality'] / 1e5
+                 # Group total mortality per MIMOSA region
+                mor_temp['MIMOSA'] = mor_temp['hierid'].apply(MIMOSA_params.get_region)
+                mortemp_grouped = mor_temp[['total']+['MIMOSA']].groupby('MIMOSA').sum()
+                 # Calculate mortality per 100,000
+                relative_mortality = mortemp_grouped['total']*1e5 / POP_MIMOSA[f'{year}']#.sum())
+                results.loc[:, (group, scenario, year)] = relative_mortality
+                print(f'{climate_model} - {group} - {scenario} - {year}')
+    #results.index = mortemp_grouped.index
+    results.to_csv(f'{folder_path}/MIMOSAMortality_allSSP_{climate_model}.csv') ### Save files
+
+
+
+'''Calculate regional mortality under SSP2'''
+results = pd.DataFrame(index=MIMOSA_params.MIMOSA.keys(), columns = pd.MultiIndex.from_product([groups, scenarios, years], names=['Age group', 'Scenario', 'Year']))
 for climate_model in climate_models_info.climate_models_dic.keys():
     for group in groups:
         mor_np = calculate_mortality_noadap.read_mortality_response(RF_path, group)
 
-        POP = d.read_csv(f'{base_path}/Main folder/GDP & POP/POP files/POP_{scenario_SSP}_{group}.csv')
+        POP = pd.read_csv(f'{base_path}/Main folder/GDP & POP/POP files/POP_{scenario_SSP}_{group}.csv')
         POP['MIMOSA'] = POP['hierid'].apply(MIMOSA_params.get_region)
         POP_MIMOSA = POP[[f"{num}" for num in years] + ['MIMOSA']].groupby('MIMOSA').sum()
 
@@ -38,5 +69,36 @@ for climate_model in climate_models_info.climate_models_dic.keys():
                 relative_mortality = mortemp_grouped['total']*1e5 / POP_MIMOSA[f'{year}'] # Calculate mortality per 100,000
                 results.loc[:, (group, scenario, year)] = relative_mortality
                 print(f'{climate_model} - {group} - {scenario} - {year}')
-    results.index = mortemp_grouped.index
-    results.to_csv(f'{folder_path}/MIMOSAMortality_NoAdaptation_{climate_model}.csv') ### Save files
+    #results.index = mortemp_grouped.index
+    results.to_csv(f'{folder_path}/MIMOSAMortality_SSP2_{climate_model}.csv') ### Save files
+
+
+
+'''Calculate Hot&Cold Mortality'''
+'''Uncomment for either hot or cold'''
+tmin = pd.read_csv(f'{base_path}/Main folder/Response functions/T_min.csv')
+results = pd.DataFrame(index=MIMOSA_params.MIMOSA.keys(), columns = pd.MultiIndex.from_product([groups, scenarios, years], names=['Age group', 'Scenario', 'Year']))
+for climate_model in climate_models_info.climate_models_dic.keys():
+    for group in groups:
+        mor_np = calculate_mortality_noadap.read_mortality_response(RF_path, group)
+        for scenario, scenario_SSP in zip(scenarios, scenarios_SSP):
+            POP = d.read_csv(f'{base_path}/Main folder/GDP & POP/POP files/POP_{scenario_SSP}_{group}.csv') 
+            POP['MIMOSA'] = POP['hierid'].apply(MIMOSA_params.get_region)
+            POP_MIMOSA = POP[[f"{num}" for num in years] + ['MIMOSA']].groupby('MIMOSA').sum()
+            
+            t_min = np.array(tmin[f'Tmin {group}'])
+
+            for year in years:
+                SSP = pd.read_csv(f'{base_path}/Climate data/Climate ensemble/{climate_model}/{scenario}/BC_{climate_model}_{scenario}_{year}.csv')  
+                mor_temp = calculate_mortality_noadap.calculate_mortality_year_hot(SSP, mor_np, t_min) 
+                #mor_temp = calculate_mortality_noadap.calculate_mortality_year_cold(SSP, mor_np, t_min)
+                mor_temp['total'] = POP[f'{year}']*mor_temp['total mortality'] / 1e5 
+                mor_temp['MIMOSA'] = mor_temp['hierid'].apply(MIMOSA_params.get_region) 
+                mortemp_grouped = mor_temp[['total']+['MIMOSA']].groupby('MIMOSA').sum()
+                relative_mortality = mortemp_grouped['total'] * 1e5 / POP_MIMOSA[f'{year}'] 
+                results.loc[:, (group, scenario, year)] = relative_mortality
+                print(f'{climate_model} - {group} - {scenario} - {year}')
+    results.to_csv(f'{folder_path}/MIMOSAMortality_Hot_{climate_model}.csv')
+    #results.to_csv(f'{folder_path}/MIMOSAMortality_Cold_{climate_model}.csv') 
+
+
