633d6e47f4c8c0585f102be7c6a481aaebb25dec
Nayeli <ditesli@users.noreply.github.com>
2025-10-28 15:54:40 +0100

    Put code in a single file

--- Burkart2022/PAF_calculations.py
+++ Burkart2022/PAF_calculations.py
@@ -5,9 +5,351 @@ This script contains the functions to run the mortality module
 import pandas as pd
 import numpy as np
 import scipy as sp
-import generate_daily_temp as gdt
 import read_files as rf
 from dataclasses import dataclass
+import xarray as xr
+
+
+### -----------------------------------------------------------------------------------------
+### Linear interpolation of population data to yearly data
+
+def map_ssp(ssp: str) -> str:
+    mapping = {
+        "SSP1": "SSP1_M",
+        "SSP2": "SSP2_CP",
+        "SSP3": "SSP3_H",
+        "SSP5": "SSP5_H"
+    }
+    try:
+        return mapping[ssp]
+    except KeyError:
+        raise ValueError(f"SSP '{ssp}' not valid. Use one of the following: {list(mapping.keys())}.")
+
+
+def get_annual_pop(wdir, scenario, years=None):
+    
+    '''Read scenario-dependent population data and interpolate it to yearly data'''
+    
+    ssp = map_ssp(scenario)
+    pop = xr.open_dataset(f'{wdir}\\data\\Socioeconomic_Data\\Population\\GPOP\\GPOP_{ssp}.nc')
+    ### Reduce resolution to 15 min
+    pop_coarse = pop.coarsen(latitude=3, longitude=3, boundary='pad').sum(skipna=False)
+    # Linearly interpolate to yearly data
+    yearly_data = pd.date_range(start='1970/01/01', end='2100/01/01', freq='YS')
+    pop_yearly = pop_coarse.interp(time=yearly_data)
+    
+    if years:
+        pop_yearly = pop_yearly.sel(time=slice(f'{years[0]}-01-01', f'{years[-1]}-01-01'))
+    
+    # if temperature_type:
+    #     # Open random ERA5 file to get temperature data grid
+    #     era5_xr = xr.open_dataset('X:\\user\\liprandicn\\Data\\ERA5\\t2m_daily\\era5_t2m_mean_day_2010.nc')
+    #     era5_xr = era5_xr.assign_coords(longitude=((era5_xr.coords['longitude'] + 180) % 360 - 180)).sortby("longitude")
+    #     # Interpolate population data to match ERA5 grid
+    #     pop_yearly = pop_yearly.interp(latitude=era5_xr.latitude, longitude=era5_xr.longitude, method='nearest')
+    
+    print(f'{scenario} population data loaded')
+    
+    return pop_yearly
+
+
+def get_all_population_data():
+    
+    pop_ssp_1 = get_annual_pop('SSP1_M')
+    pop_ssp_2 = get_annual_pop('SSP2_CP')
+    pop_ssp_3 = get_annual_pop('SSP3_H')
+    pop_ssp_5 = get_annual_pop('SSP5_H')
+
+    # Create xarray with all the SSPs scenarios
+    pop_all_ssp = xr.concat([pop_ssp_1, pop_ssp_2, pop_ssp_3, pop_ssp_5], dim = 'ssp')
+    pop_all_ssp['ssp'] = ['ssp1', 'ssp2', 'ssp3', 'ssp5']
+    # Mask with positive pop values
+    mask_positive_pop = (pop_all_ssp > 0).any(dim=['time', 'ssp'])
+    
+    return pop_all_ssp, mask_positive_pop
+
+
+### -----------------------------------------------------------------------------------------
+### Load climate files (AR6 & GCM data)
+
+def read_climate_data(wdir):
+    
+    '''Import GCM data from IMAGE land and load AR6 mean pathways
+    (this will be later replaced by the emulator) '''
+    
+    ### Import GCM data from IMAGE land 
+    gcm_diff = xr.open_dataset(f'{wdir}\\data\\Climate_Data\\GCM\\ensemble_mean_ssp585_tas_15min_diff.nc')
+    gcm_start = xr.open_dataset(f'{wdir}\\data\\Climate_Data\\GCM\\ensemble_mean_ssp585_tas_15min_start.nc')
+    gcm_end = xr.open_dataset(f'{wdir}\\data\\Climate_Data\\GCM\\ensemble_mean_ssp585_tas_15min_end.nc')
+
+    ### Import AR6 data 
+    ar6 = pd.read_csv('X:\\user\\dekkerm\\Data\\AR6_snapshots\\AR6_snapshot_Nayeli_global.csv')
+    cc_path_mean = ar6.iloc[:, 10:-1].groupby(ar6['Category']).mean()
+    
+    print('Climate data loaded')
+    
+    return gcm_diff, gcm_start, gcm_end, cc_path_mean
+
+
+### -------------------------------------------------------------------------
+### Read temperature zones
+
+def read_temperature_zones(wdir):
+    
+    ### Import ERA5 temperature zones
+    era5_tz = xr.open_dataset(f'{wdir}\\data\\Climate_Data\\ERA5\\ERA5_mean_1980-2019_land_t2m_tz.nc')
+
+    ### Convert file to numpy array
+    era5_tz = era5_tz.t2m.values
+    
+    print('Temperature zones data loaded')
+    
+    return era5_tz
+
+
+
+### -----------------------------------------------------------------------------------------
+### Load IMAGE regions and temperature zone xarray files
+
+def read_region_classification(wdir, region_class):
+    
+    ### Import ERA5 temperature zones
+    era5_tz = xr.open_dataset(f'{wdir}\\data\\Climate_Data\\ERA5\\ERA5_mean_1980-2019_land_t2m_tz.nc')
+    
+    if region_class == 'IMAGE26':
+    
+        ### Read in IMAGE region data and interpolate to match files resolution
+        region_nc = xr.open_dataset(f'{wdir}\\data\\IMAGE_regions\\GREG_30MIN.nc')
+        region_nc = region_nc.interp(longitude=era5_tz.longitude, latitude=era5_tz.latitude, method='nearest').mean(dim='time') 
+        ### Convert files to numpy arrays
+        region_nc = region_nc.GREG_30MIN.values
+        
+        # Get number of regions
+        regions_range = range(1,27)
+        
+    if region_class == 'GBD_level3':
+        
+        # Read in GBD LEVEL 3 region DATA
+        region_nc = xr.open_dataset(f'{wdir}\\data\\GBD_Data\\GBD_locations\\GBD_locations_level3.nc')
+        region_nc = region_nc.interp(longitude=era5_tz.longitude, latitude=era5_tz.latitude, method='nearest')
+        
+        # Get number of regions
+        regions_range = np.unique(region_nc.loc_id.values)[1:-1].astype(int)  # Exclude -1 and nan
+        region_nc = region_nc.loc_id.values
+    
+    print(f'{region_class} regions loaded')
+    
+    return region_nc, regions_range
+
+
+### -----------------------------------------------------------------------------------------
+### Load ERF data for Method 1 (all diseases)
+
+def open_all_diseases_data():
+    
+    ### List of all relevant diseases
+    diseases = ['ckd', 'cvd_cmp', 'cvd_htn', 'cvd_ihd', 'cvd_stroke', 'diabetes', 'inj_animal', 'inj_disaster', 'inj_drowning', 
+                'inj_homicide', 'inj_mech', 'inj_othunintent', 'inj_suicide', 'inj_trans_other', 'inj_trans_road', 'resp_copd', 'lri']
+
+    erf = pd.read_csv(f'{wdir}\\ResponseFunctions\\erf_reformatted2\\erf_ckd.csv', index_col=0)
+    erf = erf[['annual_temperature', 'daily_temperature']]
+    erf = erf.set_index(['annual_temperature', 'daily_temperature'])
+
+    for disease in diseases:
+        df = pd.read_csv(f'{wdir}\\ResponseFunctions\\erf_reformatted2\\erf_{disease}.csv', index_col=[1,2])
+        erf[disease] = df['rr']
+
+    erf = erf.reset_index()
+    erf.rename(columns={'daily_temperature':'dailyTemp', 'annual_temperature': 'meanTemp'}, inplace=True)
+
+    min_dict = erf.groupby('meanTemp')['dailyTemp'].min().to_dict()
+    max_dict = erf.groupby('meanTemp')['dailyTemp'].max().to_dict()
+    
+    print('ERF data imported')
+    
+    return diseases, erf, min_dict, max_dict
+
+
+### -----------------------------------------------------------------------------------------
+### Load ERF data for Method 2 
+
+def open_all_diseases_data_method2():
+    
+    diseases = ['ckd', 'cvd_cmp', 'cvd_htn', 'cvd_ihd', 'cvd_stroke', 'diabetes', 'resp_copd', 'lri']
+    
+    erf = {}
+    for disease in diseases:
+        erf[disease] = pd.read_csv(f'{wdir}\\ResponseFunctions\\erf_reformatted\\erf_{disease}_mean.csv', header=None).to_numpy()
+
+    return diseases, erf
+
+
+    
+### ------------------------------------------------------------------------------------------
+### Method 1.1
+
+def read_erf_data(wdir, all_diseases=True):
+    
+    """
+    Read the raw Exposure Response Functions of relevant diseases from the specified path.
+    
+    Returns:
+    - erf: Dictionary containing the ERF dataframes. Relevant diseases only
+    
+    """
+    if all_diseases:
+        disease_list = ['ckd', 'cvd_cmp', 'cvd_htn', 'cvd_ihd', 'cvd_stroke', 'diabetes', 'inj_animal', 'inj_disaster', 'inj_drowning', 
+                'inj_homicide', 'inj_mech', 'inj_othunintent', 'inj_suicide', 'inj_trans_other', 'inj_trans_road', 'resp_copd', 'lri']   
+    else:
+        disease_list = ['ckd', 'cvd_cmp', 'cvd_htn', 'cvd_ihd', 'cvd_stroke', 'diabetes', 'lri', 'resp_copd'] 
+    
+    erf_dict = {}
+
+    for disease in disease_list:
+        erf_disease = pd.read_csv(f'{wdir}\\data\\GBD_Data\\Exposure_Response_Functions\\ERF\\{disease}_curve_samples.csv', 
+                                  index_col=[0,1])
+        erf_disease.index = pd.MultiIndex.from_arrays([erf_disease.index.get_level_values(0),
+                                                       erf_disease.index.get_level_values(1).round(1)])
+        erf_dict[disease] = erf_disease
+        
+    return erf_dict, disease_list
+
+
+def get_tmrel_map(wdir, year, mean=True, random_draw=True, draw=None):
+    
+    '''Get a single TMREL draw according to the arguments of the function
+    - Mean: Calculates the mean of all draws 
+    - random_draw: Selects a random draw between the 100 available
+    - draw: Select a specific draw for all diseases (useful for propagation of uncertainty runs)
+    
+    The function:
+    1. Opens the .nc file with optimal temperatures for the selected year
+    (Available: 1990, 2010, 2020). Default for future projections: 2020
+    2. Selects a draw o calculates the mean
+    
+    Returns: 
+    1. 2-D np.array with the optimal temperature per pixel
+    
+    '''
+    # year = 2020
+    
+    tmrel = xr.open_dataset(f'{wdir}\\data\\GBD_Data\\Exposure_Response_Functions\\TMRELs_{year}.nc')
+    
+    if random_draw:
+        draw = random.randint(1,100)
+        
+    if mean:
+            tmrel = tmrel.tmrel.values.mean(axis=2)
+
+    else:
+        tmrel = tmrel.sel(draw=draw).tmrel.values
+        
+    print('TMREL data loaded')
+        
+    return tmrel
+
+
+
+###---------------------------------------------------------------------------------------------
+### Generate daily temperature data for one year and one C-Category --- Interpolate within months and adding noise
+
+def get_montlhy_nc(ccategory, year, month_start, month_end, cc_path_mean, gcm_diff, gcm_start, gcm_end):
+    
+    ### Use IMAGE land interpolation method
+    nsat_month_cc = gcm_start + (cc_path_mean.at[ccategory, f'{year}'] / gcm_diff) * gcm_end
+    
+    ### Slice selected months and delete empty variable  "time"
+    return nsat_month_cc.sel(NM=slice(month_start, month_end)).mean(dim='time', skipna=True)
+
+
+def daily_temp_interp(ccategory, year, cc_path_mean, gcm_diff, gcm_start, gcm_end, noise, noise_leap):
+    
+    ### Apply get_montlhy_nc function to near years
+    nsat_month_cc = get_montlhy_nc(ccategory, year, 1, 12, cc_path_mean, gcm_diff, gcm_start, gcm_end)
+    nsat_month_cc_pre = get_montlhy_nc(ccategory, year-1, 12, 12, cc_path_mean, gcm_diff, gcm_start, gcm_end) if year > 2000 else nsat_month_cc.sel(NM=1)
+    nsat_month_cc_pos = get_montlhy_nc(ccategory, year+1, 1, 1, cc_path_mean, gcm_diff, gcm_start, gcm_end) if year < 2100 else nsat_month_cc.sel(NM=12)
+
+    ### concat data in one xarray
+    dec_year_jan = xr.concat([nsat_month_cc_pre, nsat_month_cc, nsat_month_cc_pos], dim='NM')
+
+    ### Generate monthly dates
+    monthly_time = pd.date_range(start=f'15/12/{year-1}', end=f'15/2/{year+1}', freq='ME') - pd.DateOffset(days=15)
+    
+    ### Change NM data to monthly data and rename variable
+    dec_year_jan = dec_year_jan.assign_coords(NM=monthly_time).rename({'NM': 'time'})
+    
+    ### Interpolation (slinear for now)
+    dec_year_jan_daily = dec_year_jan.resample(time='1D').interpolate('slinear')
+    
+    ### Make Antarctica values NaN
+    dec_year_jan_daily = dec_year_jan_daily.where(dec_year_jan_daily.latitude >= -60, np.nan)
+    
+    ### Keep only data within the selected year in numpy array format
+    daily_1year = dec_year_jan_daily.sel(time=slice(f'{year}/1/1', f'{year}/12/31')).tas.values#.round(1)
+    
+    ### Add random noise
+    
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        noise = noise_leap
+        num_days = 366
+    else:
+        noise = noise
+        num_days = 365
+    
+    ### Shuffle noise array
+    np.random.shuffle(noise)
+    ### Add to daily temperature
+    daily_1year += noise
+    
+    print(f'{ccategory} - {year} daily temperatures generated')
+
+    return daily_1year, num_days
+
+
+def create_noise(std_value):
+    
+    np.random.seed(0)
+    noise_leap = np.random.normal(scale=std_value, size=(720, 1440, 366))#.round(1) 
+    ### Remove noise for 29th of February
+    noise = np.delete(noise_leap, 59, axis=2)  
+    
+    print('Temperature noise generated')
+
+    return noise, noise_leap
+
+
+### ---------------------------------------------------------------------------------------------
+### Read and process daily ERA5 Reanalysis data
+
+def daily_temp_era5(year, pop_ssp, to_array=False):
+    
+    # Read file and shift longitude coordinates
+    era5_daily = xr.open_dataset(f'X:\\user\\liprandicn\\Data\\ERA5\\t2m_daily\\era5_t2m_mean_day_{year}.nc')
+    
+    # Shift longitudinal coordinates
+    era5_daily = era5_daily.assign_coords(longitude=((era5_daily.coords['longitude'] + 180) % 360 - 180)).sortby("longitude")
+    
+    # Convert to Celsius 
+    era5_daily -= 273.15
+    
+    # Match grid with population data
+    era5_daily = era5_daily.interp(longitude=pop_ssp.longitude, latitude=pop_ssp.latitude)
+    
+    # Swap axes to match required format
+    if to_array:
+        daily_temp = era5_daily.t2m.values.swapaxes(1,2).swapaxes(0,2)
+    else: 
+        daily_temp = era5_daily.drop_vars('number')
+    
+    # Define num_days for leap year/non-leap year
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        num_days = 366
+    else:
+        num_days = 365
+        
+    print(f'ERA5 {year} daily temperatures imported')
+    
+    return daily_temp, num_days
 
 
 
@@ -535,7 +877,7 @@ def run_main_with_ccategory(wdir, ssp, years, region_class, ccategories, std_val
     gcm_diff, gcm_start, gcm_end, cc_path_mean = rf.read_climate_data(wdir)
 
     # Generate the daily temperature data variability
-    noise, noise_leap = gdt.create_noise(std_value)
+    noise, noise_leap = create_noise(std_value)
     
     #pop_ssp, image_regions, temperature_zones, tmrel, df_erf_tmrel, diseases, min_dict, max_dict 
     res = load_main_files(wdir, ssp, years, single_erf, extrap_erf, temp_max, temp_min, 
@@ -549,7 +891,7 @@ def run_main_with_ccategory(wdir, ssp, years, region_class, ccategories, std_val
 
         for year in years:
             
-            daily_temp, num_days = gdt.daily_temp_interp(ccategory, year, cc_path_mean, gcm_diff, gcm_start, gcm_end, 
+            daily_temp, num_days = daily_temp_interp(ccategory, year, cc_path_mean, gcm_diff, gcm_start, gcm_end, 
                                                          noise, noise_leap)
 
             # Select population for the corresponding year
@@ -601,7 +943,7 @@ def run_main_with_era5(wdir, ssp, years, region_class, single_erf=False, extrap_
 
     for year in years:
         
-        daily_temp, num_days = gdt.daily_temp_era5(year, res.pop_ssp, to_array=True)
+        daily_temp, num_days = daily_temp_era5(year, res.pop_ssp, to_array=True)
 
         # Select population for the corresponding year
         pop_ssp_year = res.pop_ssp.sel(time=f'{year}').mean('time').GPOP.values
