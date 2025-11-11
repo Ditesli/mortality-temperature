fce6357e8aadd393e0c9648d693fd78d7a63770a
Nayeli <ditesli@users.noreply.github.com>
2025-11-11 13:42:01 +0100

    Update temp_zones_tmrels.py

--- Burkart2022/preprocessing/temp_zones_tmrels.py
+++ Burkart2022/preprocessing/temp_zones_tmrels.py
@@ -1,27 +1,29 @@
 import utils
 
 '''
-This script calculates temperature zones and TMRELs (Theoretical Minimum Risk Exposure Levels) 
-for a specific year based on ERA5 temperature data and GBD location data.
+This script calculates temperature zones following Burkart et al. methodology.
+The script also generates raster files for the GBD locations at level 3 using the shapefile 
+available at the GBD repository: 
+and TMRELs (Theoretical Minimum Risk Exposure Levels) as raster files.
 '''
 
 # Path to main working directory
 wdir = 'X:/user/liprandicn/mt-comparison/burkart2022/'
 
+# Path to ERA5 t2m data and land-sea mask
+path_era5 = 'X:/user/liprandicn/Data/ERA5/'
+
 
 ### ---------------------------------------------------------------------------------------------------
 ### Calculate temperature zones for a specific period
 
-# Path to ERA5 t2m data and land-sea mask
-# path_era5 = 'X:/user/liprandicn/Data/ERA5/'
-
-# utils.calculate_temperature_zones(wdir, path_era5)
+utils.calculate_temperature_zones(wdir, path_era5)
 
 
 ### ---------------------------------------------------------------------------------------------------
 ### Generate raster file with GBD level 3 locations shapefile
 
-# utils.generate_raster_gbd_locations(wdir)
+utils.generate_raster_gbd_locations(wdir)
 
 ### ---------------------------------------------------------------------------------------------------
 ### Calculate TMRELs for a specific year
