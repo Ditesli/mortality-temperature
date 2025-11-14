1c7da58040c8d4db7e0a93a3e184c5a770d8cad6
Nayeli <ditesli@users.noreply.github.com>
2025-11-14 14:52:31 +0100

    Add region classification function to pop script

--- utils_common/population.py
+++ utils_common/population.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import xarray as xr
+from scipy import stats
 
 
 
@@ -97,3 +99,77 @@ def map_ssp(ssp: str) -> str:
         return mapping[ssp_normalized]
     except KeyError:
         raise ValueError(f"SSP '{ssp}' not valid. Use one of the following: {list(mapping.keys())}.")
+    
+    
+    
+def read_region_classification(wdir, region_class, temp_source):
+    
+    '''
+    Load the region classification selected (IMAGE26 or country level) and return as numpy array 
+    along with number of regions.
+    Depending on the temperature data source and the region classification type, change the resolution 
+    to either 0.5x0.5 or 0.25x0.25 degrees.
+    In case the grid has negative values (e.g., IMAGE26), set them to 0.
+    Parameters:
+    - wdir: working directory
+    - region_class: 'IMAGE26' or 'countries'
+    - temp_source: 'ERA5' or 'MS'
+    Returns:
+    - region_nc: numpy array with region classification
+    - regions_range: range or list with number of regions
+    '''
+    
+    ### Import ERA5 temperature zones
+    lats = np.arange(89.875, -90, -0.25)
+    lons = np.arange(-179.875, 180, 0.25)
+
+    ref_grid = xr.DataArray(
+        np.full((lats.size, lons.size), np.nan),
+        coords={"latitude": lats, "longitude": lons},
+        dims=["latitude", "longitude"]
+    )
+        
+    if region_class == 'IMAGE26':
+    
+        # Read in IMAGE region data and interpolate to match files resolution
+        region_nc = xr.open_dataset(wdir+'data/regions_classification/IMAGE/GREG_30MIN.nc')
+        
+        # Interpolate to match temperature data resolution and average over time dimension for ERA5 data
+        if temp_source == 'ERA5':
+            region_nc = region_nc.interp(longitude=ref_grid.longitude, 
+                                        latitude=ref_grid.latitude, 
+                                        method='nearest').mean(dim='time') 
+            
+        # Convert files to numpy arrays
+        region_nc = region_nc.GREG_30MIN.values
+        
+        # Get number of regions
+        regions_range = range(1,27)
+        
+    if region_class == 'countries':
+        
+        # Read in GBD LEVEL 3 region data: countries and territories
+        region_nc = xr.open_dataset(wdir+'data/regions_classification/GBD/GBD_locations_level3.nc')
+        
+        # Interpolate to match temperature data resolution
+        region_nc = region_nc.interp(longitude=ref_grid.longitude, 
+                                     latitude=ref_grid.latitude, 
+                                     method='nearest')
+        
+        # Get number of regions
+        regions_range = np.unique(region_nc.loc_id.values)[1:-1].astype(int)  # Exclude -1 and nan
+        region_nc = region_nc.loc_id.values
+        
+        # Reduce resolution to 0.5x0.5 degrees
+        if temp_source == 'MS':
+            
+            # Reshape array to 4D blocks of 2x2
+            arr_reshaped = region_nc.reshape(360, 2, 720, 2)
+
+            # Calculate mode over the 2x2 blocks to reduce resolution
+            region_nc = stats.mode(stats.mode(arr_reshaped, axis=3, keepdims=False).mode, axis=1, keepdims=False).mode
+    
+    # Set negative values to 0
+    region_nc = np.maximum(region_nc, 0)  
+        
+    return region_nc, regions_range
