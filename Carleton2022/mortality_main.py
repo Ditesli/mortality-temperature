96d3401092facade63c60a0985bd37d18ed4b939
Nayeli <ditesli@users.noreply.github.com>
2026-02-19 13:12:14 +0100

    Add reporting tool option

--- Carleton2022/mortality_main.py
+++ Carleton2022/mortality_main.py
@@ -7,13 +7,14 @@ with open(config_file) as f:
     config = yaml.safe_load(f)
     
 
-mf.CalculateMortality(wdir=config["wdir"], # Working directory
-                    years=range(config["start_year"], config["end_year"]), # Years range
-                    temp_dir=config["temp_dir"], # Path to climate data files
-                    gdp_dir=config["gdp_dir"], # Path to GDP data files
-                    project=config["project"], # Project name
-                    scenario=config["scenario"], # Scenario name
-                    regions="IMAGE26", # "IMAGE26" or "countries"
-                    adaptation=config["adaptation"], # Adaptation on or off
-                    IAM_format=True # If True, use IAM output format
-                    )
+mf.CalculateMortality(
+    wdir=config["wdir"], # Working directory
+    years=range(config["start_year"], config["end_year"]), # Years range
+    temp_dir=config["temp_dir"], # Path to climate data files
+    gdp_dir=config["gdp_dir"], # Path to GDP data files
+    project=config["project"], # Project name
+    scenario=config["scenario"], # Scenario name
+    regions=config["regions"],  # "IMAGE26" or "countries"
+    adaptation=config["adaptation"], # Adaptation on or off
+    reporting_tool=config["reporting_tool"], # Report on or off
+)
