ea69f6c1012d38c3159bb470219e3d854255b647
Nayeli <ditesli@users.noreply.github.com>
2026-02-26 15:09:55 +0100

    Update PAF_main.py

--- models/Burkart2022/model/PAF_main.py
+++ models/Burkart2022/model/PAF_main.py
@@ -10,8 +10,8 @@ with open(config_file) as f:
 paf.run_main(wdir=config["wdir"],   # Working directory
              temp_dir=config["temp_dir"], # Temperature data directory
              temp_source=config["temp_source"], # Temperature data source
-             ssp=config["SSP2"],   # SSP scenario
-             years=config["years"],  #  Years range
+             ssp=config["ssp"],   # SSP scenario
+             years=range(config["start_year"],config["end_year"]),  #  Years range
              region_class=config["region_class"],   # Region classification 
              draw_type=config["draw_type"],  # Mean RR or specific/random draw
              single_erf=config["single_erf"],   # Single ERF or use temperature zones
