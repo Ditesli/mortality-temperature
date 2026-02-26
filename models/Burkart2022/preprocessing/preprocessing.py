fb3ebb4fa0dc6f3ba9c168c21c823adc92817ff9
Nayeli <ditesli@users.noreply.github.com>
2026-02-26 15:44:34 +0100

    Add config.yaml and change code to process it

--- models/Burkart2022/preprocessing/preprocessing.py
+++ models/Burkart2022/preprocessing/preprocessing.py
@@ -1,32 +1,24 @@
 import utils
-
-'''
-This script calculates temperature zones following Burkart et al. methodology.
-The script also generates raster files for the GBD locations at level 3 using the shapefile 
-available at the GBD repository: 
-and TMRELs (Theoretical Minimum Risk Exposure Levels) as raster files.
-'''
-
-# Path to main working directory
-wdir = 'X:/user/liprandicn/mt-comparison/burkart2022/'
-
-# Path to ERA5 t2m data and land-sea mask
-path_era5 = 'X:/user/liprandicn/Data/ERA5/'
+from pathlib import Path
+import yaml
 
 
-### ---------------------------------------------------------------------------------------------------
-### Calculate temperature zones for a specific period
+def load_config():
+    config_path = Path(__file__).parent / "config.yaml"
+    with config_path.open() as f:
+        return yaml.safe_load(f)
 
-utils.calculate_temperature_zones(wdir, path_era5)
+        
+def main():
+    config = load_config()
+    wdir = config["wdir"]
 
+    utils.calculate_temperature_zones(wdir, config["path_era5"])
+    utils.generate_raster_gbd_locations(wdir)
 
-### ---------------------------------------------------------------------------------------------------
-### Generate raster file with GBD level 3 locations shapefile
-
-utils.generate_raster_gbd_locations(wdir)
-
-### ---------------------------------------------------------------------------------------------------
-### Calculate TMRELs for a specific year
-
-for year in [1990,2010,2020]:
-    utils.generate_tmrels_rasters(wdir, year)
+    for year in [1990, 2000, 2020]:
+        utils.generate_tmrels_rasters(wdir, year)
+        
+        
+if __name__ == "__main__":
+    main()
