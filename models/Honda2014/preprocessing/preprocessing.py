f4c30f30deb08a64007f3857c2c39d3fc1b2d28c
Nayeli <ditesli@users.noreply.github.com>
2026-03-02 14:34:31 +0100

    Rename preprocessing file

---
+++ models/Honda2014/preprocessing/preprocessing.py
@@ -0,0 +1,28 @@
+
+import utils
+from pathlib import Path
+import yaml
+
+
+def load_config():
+    config_path = Path(__file__).parent / "config.yaml"
+    with config_path.open() as f:
+        return yaml.safe_load(f)
+
+        
+def main():
+    config = load_config()
+    utils.calculate_optimal_temperature(
+        temp_dir=config["temp_dir"],
+        out_dir=config["out_dir"],
+        years=range(config["start_year"],config["end_year"]), 
+        step=config["step"],
+        temp_type="max",
+        percentile=config["percentile"]
+    )
+        
+        
+if __name__ == "__main__":
+    main()
+
+
