5c6910b1c194a94b2c85a5eede0ebaef70acd092
Nayeli <ditesli@users.noreply.github.com>
2026-03-02 14:48:03 +0100

    Update main file

---
+++ models/Honda2014/mortality_main.py
@@ -0,0 +1,19 @@
+import mortality_functions as mf
+import yaml
+from pathlib import Path
+
+config_file = Path(__file__).parent / "config.yaml"
+with open(config_file) as f:
+    config = yaml.safe_load(f)
+
+
+mf.run_main(wdir=config["wdir"],   # Working directory
+            temp_dir=config["temp_dir"], # ERA5 directory
+            project=config["project"],
+            scenario=config["scenario"],   # SSP scenario
+            years=range(config["start_year"], config["end_year"]),  #  Years range
+            region_class=config["region_class"],   # Region classification 
+            optimal_range=config["optimal_range"],
+            extrap_erf=config["extrap_erf"],   # Extrapolate ERF(s)
+            temp_max=config["temp_max"],   # Minimum temperature to extrapolate to
+            )
