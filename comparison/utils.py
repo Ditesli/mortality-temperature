dafe266c4148e95829acc95b1b8c2b50bdd7da82
Nayeli <ditesli@users.noreply.github.com>
2026-06-22 10:42:53 +0200

    Delete old functions add function so code can read Carleton draws

--- comparison/utils.py
+++ comparison/utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import glob
+import glob, re
 import xarray as xr
 import matplotlib.pyplot as plt
 
@@ -180,53 +180,31 @@ def LoadMortalityDraws(wdir, filename, region_type, region, t_type, cause, age_g
     else:
         print("Files not found.")
     
-    da_selected = ds.sel(
-    region_type=region_type,
-    region=region,
-    t_type=t_type,
-    cause=cause,
-    age_group=age_group
-    )[variable]
+    filters = {
+    "region_type":region_type,
+    "region":region,
+    "t_type":t_type,
+    "age_group":age_group
+    }
+    
+    if "cause" in ds.variables:
+        filters["cause"] = cause
 
 
-    da_mean = da_selected.mean(dim=["draw", "var_mor"])
-    da_p025 = da_selected.quantile(0.025, dim=["draw", "var_mor"])
-    da_p975 = da_selected.quantile(0.975, dim=["draw", "var_mor"])
+    da_selected = ds.set_index(geo=["region_type", "region"]).sel(**filters)[variable]
+
+    dims = ( 
+        ["draw"]
+        if re.search(r"carleton", filename.lower())
+        else ["draw", "var_mor"]
+    )
+
+    da_mean = da_selected.mean(dim=dims)
+    da_p025 = da_selected.quantile(0.025, dim=dims)
+    da_p975 = da_selected.quantile(0.975, dim=dims)
     
     return da_mean, da_p025, da_p975
-
-
-
-def LoadCarletonMortality(wdir, filename, years, region, temp_type, unit, age_group, cause):
-    
-    draws = {}
-    i = 1
-    
-    for draw in range(1,5):
-        carleton_counter = f"{filename}_{draw}"
-        draws[i] = LoadMortality(wdir, carleton_counter, years, region, temp_type, unit, age_group, cause, None, None)
-        i += 1
         
-    carleton_df = pd.concat(draws.values(), ignore_index=True)
-    
-    c_mean = carleton_df.mean(axis=0)
-    c_p025 = carleton_df.quantile(q=0.025, axis=0, interpolation="linear")
-    c_p975 = carleton_df.quantile(q=0.975, axis=0, interpolation="linear")
-    
-    return c_mean, c_p025, c_p975
-        
-        
-# wdir = "X:\\user\\liprandicn\\mt-comparison\\"
-# temp_type = "heat"
-# unit = "total"
-# age_group = "oldest"
-# region = "World"
-# years = range(1980,2024)
-# cause = "All causes"
-# # burkart_counter = "burkart2022/output/ComparisonBurkart/MOR_ComparisonBurkart_SSP2_ERA5_IMAGE_1980-2023_counterfactual"
-# carleton_file = "carleton2022/output/ComparisonCarletonCounter/IMAGE/MOR_ComparisonCarletonCounter_SSP2_ERA5_NoAdap_1980-2023"
-
-# carleton_counter = LoadCarletonMortality(wdir, carleton_file, years, region, temp_type, unit, age_group, cause)
 
 
 def LoadBallesterScatter(wdir):
@@ -283,36 +261,6 @@ def LoadBallesterScatter(wdir):
 
 
 
-def WeightsAndCountsForKDE(tmrel, era5_tz, pop_ssp_year, tz):
-    
-    """
-    Function to get counts and weights parameters for a Kernel Density Estimation
-    """
-    
-    # Boolean mask for grid cells in the current temperature zone
-    mask = era5_tz.t2m.values == tz
-
-    # Extract TMREL values at those grid points (across all draws)
-    values = tmrel.tmrel.values[mask, :].reshape(-1)
-
-    # Repeat population weights for each draw
-    pop_weights = np.repeat(pop_ssp_year.GPOP.values[mask], tmrel.dims['draw'])
-
-    # Filter out NaNs
-    valid = ~np.isnan(values) & ~np.isnan(pop_weights)
-    values = values[valid]
-    pop_weights = pop_weights[valid]
-
-    # Normalize population weights
-    pop_weights = pop_weights / pop_weights.sum()
-
-    counts = values
-    weights = pop_weights
-    
-    return counts, weights
-
-
-
 def StylizeAxes(ax, *, 
                 xscale=None,
                 yscale=None,
@@ -363,7 +311,6 @@ def StylizeAxes(ax, *,
             ax.spines[spine].set_visible(is_visible)
     
     return ax
-
 
 
 
@@ -419,130 +366,3 @@ def StylizePlot(*,
         for spine_edge in despine:
             if spine_edge in ax.spines:
                 ax.spines[spine_edge].set_visible(False)
-
-
-
-def load_burkart(wdir, regions_class, output_type, gbd_df, temp_type, age_group, continents):
-    
-    '''
-    Load present day output data calculated using the method
-    prescribed in Burkart et al., (2022)
-    '''
-    
-    app = pd.read_csv(f'{wdir}\\burkart2022\\output\\paf_era5_GBD_level3_all-dis_{output_type}2000-2019.csv', header=[0,1,2], index_col=[0])
-    
-    gbd_ = gbd_df[
-        (gbd_df['cause_name'].isin(diseases.values())) &
-        (gbd_df['sex_name'] == 'Both') &
-        (gbd_df['year'].isin(range(2000, 2020))) &
-        (gbd_df['age_name'].isin(age_group)) &
-        (gbd_df['location_name'] != 'Global')][['location_id', 'location_name', 'cause_name', 'year', 'val', 'upper', 'lower']]	
-    gbd_['year']=gbd_['year'].astype(int)
-        
-    # Select mortality from different temperature types
-    app = app.xs(temp_type, level=2, axis=1)
-    
-    # Stack dataframe to have location, year, cause_key as index and paf as column
-    app = app.stack(level=[0,1], 
-                    future_stack=True).reset_index().rename(columns={'level_0':'location_id', 'level_1':'year', 'level_2':'cause_key', 0:'paf'})
-    
-    # Map diseases names with keys
-    app['cause_name'] = app['cause_key'].map({k:v for k,v in diseases.items()})
-    app['year'] = app['year'].astype(int)
-    
-    # Merge with mortality data
-    app = app.merge(gbd_, on=['location_id', 'cause_name', 'year'], how='left')
-    
-    # Calculate deaths attributable to temperature
-    app['mean'] = app['val'] * app['paf'] 
-    app['upper_est'] = app['upper'] * app['paf'] 
-    app['lower_est'] = app['lower'] * app['paf'] 
-    app = app[['year', 'cause_name', 'location_name', 'location_id', 'mean', 'upper_est', 'lower_est']]  
-    
-    if continents == True:
-        # Merge with regions classification
-        app = app.merge(regions_class[['gbd_location_id', 'continents']].drop_duplicates(), left_on='location_id', right_on='gbd_location_id', how='left')
-        app = app[['continents', 'year', 'mean', 'upper_est', 'lower_est']].groupby(['year','continents']).sum().reset_index()
-        app = app.groupby(['continents']).mean().reset_index()
-        
-    if continents == False:
-        app = app[['location_name', 'location_id', 'year', 'mean', 'upper_est', 'lower_est']].groupby(['year','location_name', 'location_id']).sum().reset_index()
-        app = app.groupby(['location_name', 'location_id']).mean().reset_index()
-    
-    return app
-
-
-
-def load_carleton(wdir, regions_class, t_type, age_group=None):
-    
-    '''
-    Load present day output data calculated using the method
-    prescribed in Carleton et al., (2022)
-    '''
-
-    # Load output file
-    carleton_df = pd.read_csv(f'{wdir}\\Carleton2022\\output\\mortality_ERA5_gbd-level3_2000-2019.csv', index_col=[0,1,2,3])
-    # Select a scenario (SSP2 only for present day), and a temperature type
-    
-    # Select scenario, temperature type and, if specified, age group
-    if age_group is None:
-        carleton_df = carleton_df.xs('SSP2', level=0).xs(t_type, level=1).reset_index().groupby('gbd_level3').sum().iloc[:,1:]
-        
-    else:
-        carleton_df = carleton_df.xs('SSP2', level=0).xs(age_group, level=0).xs(t_type, level=0).reset_index().groupby('gbd_level3').sum().iloc[:,1:]
-    
-    # Group countries by continent
-    carleton_df = regions_class[['gbd_level3', 'continents']].set_index('gbd_level3').merge(carleton_df, left_index=True, right_index=True).reset_index()
-    carleton_df = carleton_df.iloc[:,1:].groupby('continents').mean().mean(axis=1).reset_index()
-    
-    carleton_df.rename(columns={0:'mean'}, inplace=True)
-    
-    # Define upper an lower estimation as the mean for plotting
-    carleton_df['upper_est'] = carleton_df['mean'] 
-    carleton_df['lower_est'] = carleton_df['mean']
-    
-    return carleton_df
-
-
-
-def load_honda(wdir, regions_class, gbd_df, ot_type, temp_type, period, merging_type):
-    
-    '''
-    Load present day output data calculated using the method
-    prescribed in Burkart et al., (2022)
-    '''
-    
-    honda_df = pd.read_csv(f'{wdir}\\honda2014\\output\\paf_era5_GBD_level3_{period}_ot-{ot_type}.csv', index_col=0, header=[0,1])
-
-    honda_df = honda_df.xs(temp_type, level=1, axis=1)
-    
-    gbd_ = gbd_df[
-        (gbd_df['cause_name']=='All causes') &
-        (gbd_df['sex_name'] == 'Both') &
-        (gbd_df['year'].isin(range(2000, 2020))) &
-        (gbd_df['age_name'].isin(['65-69 years', '70-74 years', '75-79 years', '85+ years'])) &
-        (gbd_df['location_name'] != 'Global')][['location_id', 'location_name', 'cause_name', 'year', 'val', 'upper', 'lower']]	
-    gbd_['year']=gbd_['year'].astype(int)
-    gbd_ = gbd_.groupby(['location_id', 'location_name', 'cause_name', 'year']).sum().reset_index()
-
-    
-    # Stack dataframe to have location, year, cause_key as index and paf as column
-    app = honda_df.stack(level=[0], 
-                    future_stack=True).reset_index().rename(columns={'level_0':'location_id', 'level_1':'year', 0:'paf'})
-    app['year'] = app['year'].astype(int)
-
-    # Merge with mortality data
-    app = app.merge(gbd_, on=['location_id', 'year'], how='left')
-
-    # Calculate deaths attributable to temperature
-    app['mean'] = app['val'] * app['paf'] * 0.88
-    app['upper_est'] = app['upper'] * app['paf'] * 0.88
-    app['lower_est'] = app['lower'] * app['paf']  * 0.88
-    app = app[['year', 'location_name', 'mean', 'upper_est', 'lower_est']]  
-
-    # Merge with regions classification
-    app = app.merge(regions_class[['gbd_level3', merging_type]], left_on='location_name', right_on='gbd_level3', how='left').drop_duplicates()
-    app = app.groupby(['year', merging_type]).sum().reset_index()[[merging_type, 'year', 'mean', 'upper_est', 'lower_est']]
-    app = app.groupby([merging_type]).mean().reset_index()
-       
-    return app
