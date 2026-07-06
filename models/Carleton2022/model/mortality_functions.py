7a38aba76d910158c4063374132f9a68a08c9591
Nayeli <ditesli@users.noreply.github.com>
2026-07-06 15:01:43 +0200

    Update mortality_functions.py

--- models/Carleton2022/model/mortality_functions.py
+++ models/Carleton2022/model/mortality_functions.py
@@ -242,9 +242,39 @@ class LoadInputData:
         )
     
 
-        ImportGammaCoefficients(sets)
-        
-        ImportPopulationData(sets)    
+
+@dataclass
+class BaselineERFsInputs:
+    
+    """
+    Container for all input data required to generate the baseline ERFs (ERFs for the 
+    "present day", and as the basis to generate the ERFs with adaptation).
+    
+    Attributes
+    ----------
+    erfs_t0: any
+        Dictionary with each age group's ERFs per impact region.
+    tmin_t0: any
+        Dictionary with the three 1-d arrays corresponding to the minimum temperature (MMT).
+    image_shares: any
+        GDPpc shares of regions within an IMAGE region. Used to downscale GDPpc.
+    country_shares: any
+        GDPpc shares of regions within a country. Used to downscale GDPpc.
+    image_gdppc: any
+        GDPpc per IMAGE region. Used only in IMAGE sscenarios.
+    daily_temp_t0: pd.DataFrame
+        DataFrame with daily "present day" temperature data for the counterfactual part.
+    """
+
+    erfs_t0: any
+    tmin_t0: any
+    image_shares: any
+    country_shares: any
+    image_gdppc: any
+    daily_temp_t0: pd.DataFrame
+    
+    
+    def from_sets(sets, fls):
         
         # Import present day covariates
         print("[1.5] Loading 'baseline' Exposure Response Functions...")
