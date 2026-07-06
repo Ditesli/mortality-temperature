a94d4ccae481e3804d443cb096e8145f80168d40
Nayeli <ditesli@users.noreply.github.com>
2026-07-06 15:25:28 +0200

    Update mortality_functions.py

--- models/Carleton2022/model/mortality_functions.py
+++ models/Carleton2022/model/mortality_functions.py
@@ -1172,8 +1172,8 @@ def DailyTemperature2IR(sets, year, ir, spatial_relation):
         daily_temperature = MSTemperature2IR(
             temp=daily_temperature, 
             year=year, 
-            sets=sets
-            )
+            ir=ir, 
+            spatial_relation=spatial_relation)
     
     return daily_temperature.astype(np.float32)
 
