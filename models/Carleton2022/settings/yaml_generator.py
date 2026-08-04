f81ba7808f8ff12710f71636e1903c0693039d43
Nayeli <ditesli@users.noreply.github.com>
2026-08-04 13:20:33 +0200

    Update yaml_generator.py

--- models/Carleton2022/settings/yaml_generator.py
+++ models/Carleton2022/settings/yaml_generator.py
@@ -16,18 +16,18 @@ def generate_config_yaml(path, scenario, project):
     # Define data
     data = {
         "wdir": "X:/user/liprandicn/Projects/mt-comparison/models/carleton2022",
-        "temp_dir":f"X:\\user\\dekkerm\\IMAGE_environments\\IMPACTS\\3_IMAGE_land\\scen\\{scenario}\\netcdf",
-            # f"X:/user/dekkerm/IMAGE_environments/IMPACTS/Z_Emulator_Standalone_Tool/{scenario_image_land}/netcdf",
-        "gdp_dir": f"X:/user/dekkerm/IMAGE_environments/IMPACTS/2_TIMER/outputlib/TIMER_3_5/IMPACTS/{scenario[:9]}_ERA_AllImpacts/indicators/Economy/GDPpc_incl_impacts.out",
-            # f"X:/user/dekkerm/IMAGE_environments/IMPACTS/2_TIMER/outputlib/TIMER_3_5/IMPACTS/{scenario}/indicators/Economy/GDPpc_incl_impacts.out",
+        "temp_dir": f"X:/user/dekkerm/IMAGE_environments/IMPACTS/Z_Emulator_Standalone_Tool/{scenario_image_land}/netcdf",
+           
+        "gdp_dir": f"X:/user/dekkerm/IMAGE_environments/IMPACTS/2_TIMER/outputlib/TIMER_3_5/IMPACTS/{scenario}/indicators/Economy/GDPpc_incl_impacts.out",
+            
         "start_year": 2000,
-        "end_year": 2050,
+        "end_year": 2100,
         "project": project,
         "scenario": scenario,
         "adaptation": True,
         "counterfactual": True,
         "draw": "mean",
-        "reporting_tool": False,#"X:/user/dekkerm/IMAGE_environments/IMPACTS/7_Reporting_Tool/outxlsx",
+        "reporting_tool": "X:/user/dekkerm/IMAGE_environments/IMPACTS/7_Reporting_Tool/outxlsx",
         "dask_on":True
     }
 
@@ -40,6 +40,53 @@ def generate_config_yaml(path, scenario, project):
     print(f"{scenario}.yaml file generated")
 
 
+
+
+SPARCCLE_extra = [
+    'SSP1_ML_ERA_AllImpacts',
+    'SSP1_ML_ERA_AllImpacts_extra',
+    'SSP1_ML_ERA_NoEcon',
+    'SSP1_ML_ERA_NoEcon_extra',
+    'SSP1_ML_ERA_NoImpacts',
+    'SSP1_ML_ERA_NoImpacts_extra',
+    'SSP1_M_CP_ERA_AllImpacts',
+    'SSP1_M_CP_ERA_AllImpacts_extra',
+    'SSP1_M_CP_ERA_NoEcon',
+    'SSP1_M_CP_ERA_NoEcon_extra',
+    'SSP1_M_CP_ERA_NoImpacts',
+    'SSP1_M_CP_ERA_NoImpacts_extra',
+    'SSP1_VLLO_ERA_AllImpacts',
+    'SSP1_VLLO_ERA_NoEcon',
+    'SSP1_VLLO_ERA_NoImpacts',
+    'SSP1_VLLO_ERA_NoImpacts_extra',
+    'SSP1_VLLO_STS1_AllImpacts',
+    'SSP1_VLLO_STS1_AllImpacts_extra',
+    'SSP2_ML_ERA_AllImpacts',
+    'SSP2_ML_ERA_AllImpacts_extra',
+    'SSP2_ML_ERA_NoEcon',
+    'SSP2_ML_ERA_NoEcon_extra',
+    'SSP2_ML_ERA_NoImpacts',
+    'SSP2_ML_ERA_NoImpacts_extra',
+    'SSP2_M_CP_ERA_AllImpacts',
+    'SSP2_M_CP_ERA_AllImpacts_extra',
+    'SSP2_M_CP_ERA_NoEcon',
+    'SSP2_M_CP_ERA_NoEcon_extra',
+    'SSP2_M_CP_ERA_NoImpacts',
+    'SSP2_M_CP_ERA_NoImpacts_extra',
+    'SSP2_VLLO_ERA_AllImpacts',
+    'SSP2_VLLO_ERA_AllImpacts_extra',
+    'SSP2_VLLO_ERA_NoImpacts',
+    'SSP2_VLLO_ERA_NoImpacts_extra', 
+    'SSP2_VLLO_STS1_AllImpacts',
+    'SSP2_VLLO_STS1_AllImpacts_extra',
+    'SSP3_H_ERA_AllImpacts',
+    'SSP3_H_ERA_AllImpacts_extra',
+    'SSP3_H_ERA_NoEcon',
+    'SSP3_H_ERA_NoEcon_extra',
+    'SSP3_H_STS3_NoEcon',
+    'SSP3_H_STS3_NoEcon_extra',
+    'SSP2_M_CP_ERA_NoImpacts'
+    ]
 
 
 project="SPARCCLE"
@@ -73,53 +120,6 @@ for scenario in SPARCCLE_extra:
 #     "SSP2_VLLO_STS1_AllImpacts",
 #     "SSP2_VLLO_ERA_AllImpacts",
 #     "SSP1_VLLO_ERA_NoEcon"
-#     ]
-
-
-# SPARCCLE_extra = [
-#     'SSP1_ML_ERA_AllImpacts',
-#     'SSP1_ML_ERA_AllImpacts_extra',
-#     'SSP1_ML_ERA_NoEcon',
-#     'SSP1_ML_ERA_NoEcon_extra',
-#     'SSP1_ML_ERA_NoImpacts',
-#     'SSP1_ML_ERA_NoImpacts_extra',
-#     'SSP1_M_CP_ERA_AllImpacts',
-#     'SSP1_M_CP_ERA_AllImpacts_extra',
-#     'SSP1_M_CP_ERA_NoEcon',
-#     'SSP1_M_CP_ERA_NoEcon_extra',
-#     'SSP1_M_CP_ERA_NoImpacts',
-#     'SSP1_M_CP_ERA_NoImpacts_extra',
-#     'SSP1_VLLO_ERA_AllImpacts',
-#     'SSP1_VLLO_ERA_NoEcon',
-#     'SSP1_VLLO_ERA_NoImpacts',
-#     'SSP1_VLLO_ERA_NoImpacts_extra',
-#     'SSP1_VLLO_STS1_AllImpacts',
-#     'SSP1_VLLO_STS1_AllImpacts_extra',
-#     'SSP2_ML_ERA_AllImpacts',
-#     'SSP2_ML_ERA_AllImpacts_extra',
-#     'SSP2_ML_ERA_NoEcon',
-#     'SSP2_ML_ERA_NoEcon_extra',
-#     'SSP2_ML_ERA_NoImpacts',
-#     'SSP2_ML_ERA_NoImpacts_extra',
-#     'SSP2_M_CP_ERA_AllImpacts',
-#     'SSP2_M_CP_ERA_AllImpacts_extra',
-#     'SSP2_M_CP_ERA_NoEcon',
-#     'SSP2_M_CP_ERA_NoEcon_extra',
-#     'SSP2_M_CP_ERA_NoImpacts',
-#     'SSP2_M_CP_ERA_NoImpacts_extra',
-#     'SSP2_VLLO_ERA_AllImpacts',
-#     'SSP2_VLLO_ERA_AllImpacts_extra',
-#     'SSP2_VLLO_ERA_NoImpacts',
-#     'SSP2_VLLO_ERA_NoImpacts_extra', 
-#     'SSP2_VLLO_STS1_AllImpacts',
-#     'SSP2_VLLO_STS1_AllImpacts_extra',
-#     'SSP3_H_ERA_AllImpacts',
-#     'SSP3_H_ERA_AllImpacts_extra',
-#     'SSP3_H_ERA_NoEcon',
-#     'SSP3_H_ERA_NoEcon_extra',
-#     'SSP3_H_STS3_NoEcon',
-#     'SSP3_H_STS3_NoEcon_extra',
-#     'SSP2_M_CP_ERA_NoImpacts'
 #     ]
 
 
