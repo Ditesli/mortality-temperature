da4c5ea9c7a43a41f3bd70a35cf9dcbb13129be7
Nayeli <ditesli@users.noreply.github.com>
2026-08-06 17:27:21 +0200

    Update yaml_generator.py

--- models/Carleton2022/settings/yaml_generator.py
+++ models/Carleton2022/settings/yaml_generator.py
@@ -1,98 +1,120 @@
-import os
-import yaml
+import os, yaml
 
 
 def generate_config_yaml(path, scenario, project):
 
-
-    filename = f"{scenario}.yaml"
-    path = os.path.join(path+f"\\{project}", filename)
-    
     if "extra" in scenario:
         scenario_image_land = scenario[:-6]
     else:
         scenario_image_land=scenario
 
-    # Define data
-    data = {
-        "wdir": "X:/user/liprandicn/Projects/mt-comparison/models/carleton2022",
-        "temp_dir": f"X:/user/dekkerm/IMAGE_environments/IMPACTS/Z_Emulator_Standalone_Tool/{scenario_image_land}/netcdf",
-           
-        "gdp_dir": f"X:/user/dekkerm/IMAGE_environments/IMPACTS/2_TIMER/outputlib/TIMER_3_5/IMPACTS/{scenario}/indicators/Economy/GDPpc_incl_impacts.out",
-            
-        "start_year": 2000,
-        "end_year": 2100,
-        "project": project,
-        "scenario": scenario,
-        "adaptation": True,
-        "counterfactual": True,
-        "draw": "mean",
-        "reporting_tool": "X:/user/dekkerm/IMAGE_environments/IMPACTS/7_Reporting_Tool/outxlsx",
-        "dask_on":True
-    }
+    for climvar in ["ssp245_r1", "ssp245_r2", "ssp245_r3", "ssp370_r1", "ssp370_r2", "ssp370_r3", "ssp585_r1", "ssp585_r2", "ssp585_r3"]:
+        
+        # Define data
+        data = {
+            "wdir": "X:/user/liprandicn/Projects/mt-comparison/models/carleton2022",
+            "temp_dir": f"X:\\user\\liprandicn\\data\\IMAGE_Temperatures\\ScenarioMIP7\\ACCESS-CM2_{climvar}i1p1f1\\{scenario}\\netcdf",
+            # f"X:/user/dekkerm/IMAGE_environments/IMPACTS/Z_Emulator_Standalone_Tool/{scenario_image_land}/netcdf", 
+            "gdp_dir": f"X:\\user\\boerdhs\\Projects\\ScenarioMIP\\2_TIMER\\outputlib\\TIMER_3_4\\ScenarioMIP\\SSP1_VLLO\\tuss\\global/GDP_ppp.scn",
+            "start_year": 2000,
+            "end_year": 2100,
+            "project": project,
+            "scenario": scenario+"_"+climvar,#+"_NoAdap",
+            "adaptation": True,
+            "counterfactual": True,
+            "draw": "LHScut_50_0_p25-p75",
+            "reporting_tool": False,
+                # "X:/user/dekkerm/IMAGE_environments/IMPACTS/7_Reporting_Tool/outxlsx",
+            "dask_on":True,
+            "stochastic": True
+        }
+        
 
-    # Save YAML
-    with open(path, "w", encoding="utf-8") as file:
-        yaml.dump(
-            data, file, default_flow_style=False, sort_keys=False
-        )
+        file_path = os.path.join(path, project, f"{data['scenario']}.yaml")
 
-    print(f"{scenario}.yaml file generated")
+        # Save YAML
+        with open(file_path, "w", encoding="utf-8") as file:
+            yaml.dump(
+                data, file, default_flow_style=False, sort_keys=False
+            )
 
+        print(f"{scenario}.yaml file generated")
+        
+        
 
+SCENARIOS = []
 
-
-SPARCCLE_extra = [
-    'SSP1_ML_ERA_AllImpacts',
-    'SSP1_ML_ERA_AllImpacts_extra',
-    'SSP1_ML_ERA_NoEcon',
-    'SSP1_ML_ERA_NoEcon_extra',
-    'SSP1_ML_ERA_NoImpacts',
-    'SSP1_ML_ERA_NoImpacts_extra',
-    'SSP1_M_CP_ERA_AllImpacts',
-    'SSP1_M_CP_ERA_AllImpacts_extra',
-    'SSP1_M_CP_ERA_NoEcon',
-    'SSP1_M_CP_ERA_NoEcon_extra',
-    'SSP1_M_CP_ERA_NoImpacts',
-    'SSP1_M_CP_ERA_NoImpacts_extra',
-    'SSP1_VLLO_ERA_AllImpacts',
-    'SSP1_VLLO_ERA_NoEcon',
-    'SSP1_VLLO_ERA_NoImpacts',
-    'SSP1_VLLO_ERA_NoImpacts_extra',
-    'SSP1_VLLO_STS1_AllImpacts',
-    'SSP1_VLLO_STS1_AllImpacts_extra',
-    'SSP2_ML_ERA_AllImpacts',
-    'SSP2_ML_ERA_AllImpacts_extra',
-    'SSP2_ML_ERA_NoEcon',
-    'SSP2_ML_ERA_NoEcon_extra',
-    'SSP2_ML_ERA_NoImpacts',
-    'SSP2_ML_ERA_NoImpacts_extra',
-    'SSP2_M_CP_ERA_AllImpacts',
-    'SSP2_M_CP_ERA_AllImpacts_extra',
-    'SSP2_M_CP_ERA_NoEcon',
-    'SSP2_M_CP_ERA_NoEcon_extra',
-    'SSP2_M_CP_ERA_NoImpacts',
-    'SSP2_M_CP_ERA_NoImpacts_extra',
-    'SSP2_VLLO_ERA_AllImpacts',
-    'SSP2_VLLO_ERA_AllImpacts_extra',
-    'SSP2_VLLO_ERA_NoImpacts',
-    'SSP2_VLLO_ERA_NoImpacts_extra', 
-    'SSP2_VLLO_STS1_AllImpacts',
-    'SSP2_VLLO_STS1_AllImpacts_extra',
-    'SSP3_H_ERA_AllImpacts',
-    'SSP3_H_ERA_AllImpacts_extra',
-    'SSP3_H_ERA_NoEcon',
-    'SSP3_H_ERA_NoEcon_extra',
-    'SSP3_H_STS3_NoEcon',
-    'SSP3_H_STS3_NoEcon_extra',
-    'SSP2_M_CP_ERA_NoImpacts'
-    ]
-
-
-project="SPARCCLE"
-path="X:\\user\\liprandicn\\projects\\mt-comparison\\data\\yaml_settings"
-for scenario in SPARCCLE_extra:
+project="Project"
+path="Path"
+for scenario in SCENARIOS:
     generate_config_yaml(path, scenario, project)
+    
+    
+
+# SCENARIOMIP7 = [
+#   "SSP1_L",
+#   "SSP1_M",
+#   "SSP1_M_CP",
+#   "SSP1_ML",
+#   "SSP1_VLHO",
+#   "SSP1_VLLO",
+#   "SSP2_L",
+#   "SSP2_M",
+#   "SSP2_M_CP",
+#   "SSP2_ML",
+#   "SSP2_VLHO",
+#   "SSP2_VLLO",
+#   "SSP3_H",
+#   "SSP5_H",
+#   "SSP5_HL",
+# ]
+
+
+# SPARCCLE_extra = [
+#     'SSP1_ML_ERA_AllImpacts',
+#     'SSP1_ML_ERA_AllImpacts_extra',
+#     'SSP1_ML_ERA_NoEcon',
+#     'SSP1_ML_ERA_NoEcon_extra',
+#     'SSP1_ML_ERA_NoImpacts',
+#     'SSP1_ML_ERA_NoImpacts_extra',
+#     'SSP1_M_CP_ERA_AllImpacts',
+#     'SSP1_M_CP_ERA_AllImpacts_extra',
+#     'SSP1_M_CP_ERA_NoEcon',
+#     'SSP1_M_CP_ERA_NoEcon_extra',
+#     'SSP1_M_CP_ERA_NoImpacts',
+#     'SSP1_M_CP_ERA_NoImpacts_extra',
+#     'SSP1_VLLO_ERA_AllImpacts',
+#     'SSP1_VLLO_ERA_NoEcon',
+#     'SSP1_VLLO_ERA_NoImpacts',
+#     'SSP1_VLLO_ERA_NoImpacts_extra',
+#     'SSP1_VLLO_STS1_AllImpacts',
+#     'SSP1_VLLO_STS1_AllImpacts_extra',
+#     'SSP2_ML_ERA_AllImpacts',
+#     'SSP2_ML_ERA_AllImpacts_extra',
+#     'SSP2_ML_ERA_NoEcon',
+#     'SSP2_ML_ERA_NoEcon_extra',
+#     'SSP2_ML_ERA_NoImpacts',
+#     'SSP2_ML_ERA_NoImpacts_extra',
+#     'SSP2_M_CP_ERA_AllImpacts',
+#     'SSP2_M_CP_ERA_AllImpacts_extra',
+#     'SSP2_M_CP_ERA_NoEcon',
+#     'SSP2_M_CP_ERA_NoEcon_extra',
+#     'SSP2_M_CP_ERA_NoImpacts',
+#     'SSP2_M_CP_ERA_NoImpacts_extra',
+#     'SSP2_VLLO_ERA_AllImpacts',
+#     'SSP2_VLLO_ERA_AllImpacts_extra',
+#     'SSP2_VLLO_ERA_NoImpacts',
+#     'SSP2_VLLO_ERA_NoImpacts_extra', 
+#     'SSP2_VLLO_STS1_AllImpacts',
+#     'SSP2_VLLO_STS1_AllImpacts_extra',
+#     'SSP3_H_ERA_AllImpacts',
+#     'SSP3_H_ERA_AllImpacts_extra',
+#     'SSP3_H_ERA_NoEcon',
+#     'SSP3_H_ERA_NoEcon_extra',
+#     'SSP3_H_STS3_NoEcon',
+#     'SSP3_H_STS3_NoEcon_extra',
+#     'SSP2_M_CP_ERA_NoImpacts'
+#     ]
 
 
 # SPARCCLE_2round = [
