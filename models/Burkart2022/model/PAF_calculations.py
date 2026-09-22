148c85c1d4100a137a2aa70b40a217160612f2d9
Nayeli <ditesli@users.noreply.github.com>
2026-09-22 13:17:10 +0200

    Update output paths

--- models/Burkart2022/model/PAF_calculations.py
+++ models/Burkart2022/model/PAF_calculations.py
@@ -233,9 +233,9 @@ class LoadInputData:
         
         print(f"[1.3] Calculating population per impact region for SSP: {ssp}...")
         pop_region = pop.IMAGEPopulation2Regions(
-            shp_dir=os.path.dirname(sets.wdir)+"/data/GBD/GBD_locations/gbd_shapefiles/", 
+            shp_dir=os.path.dirname(os.path.dirname(sets.wdir))+"/data/GBD/GBD_locations/gbd_shapefiles/", 
             shp_name="GBD_shapefile",
-            pop_dir=os.path.dirname(sets.wdir)+"/data", 
+            pop_dir=os.path.dirname(os.path.dirname(sets.wdir))+"/data", 
             ssp="SSP2",
             years=sets.years)   
         
